80000b254f8ef9c5b970d47ab90ec4f5eb117ad7
Abid Hasan Abir <abid.7.tm@gmial.com>
2024-11-15 16:24:16 +0600

    send data one to another component and show them in the UI also use spared oprator for copy previous data and add new data to the state

--- recape-rest-countries/src/components/Countries/Countries.jsx
+++ recape-rest-countries/src/components/Countries/Countries.jsx
@@ -2,7 +2,9 @@ import { useEffect, useState } from "react";
 import Country from "../Country/Country";
 
 const Countries = () => {
+    
     const [countires, setCountries] = useState([]);
+    const [visitedCoutries , setVisitedCoutries] = useState([]);
 
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
@@ -10,12 +12,25 @@ const Countries = () => {
             .then(data => setCountries(data))
     }, []);
 
+    const handleVisitedCountries = country =>{
+        const newVisitedCountries = [...visitedCoutries,country];
+        setVisitedCoutries(newVisitedCountries);
+    }
+
     return (
         <div className="">
             <h3>Countries : {countires.length}</h3>
+            <div>
+                <h4>Visited Country List : {visitedCoutries.length}</h4>
+                <ul>
+                    {
+                        visitedCoutries.map(visitedCountry => <li key={visitedCountry.cca2}>{visitedCountry?.name?.common}</li>)
+                    }
+                </ul>
+            </div>
             <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)' }}>
                 {
-                    countires.map(country => <Country key={country.cca2} country={country}></Country>)
+                    countires.map(country => <Country handleVisitedCountries={handleVisitedCountries} key={country.cca2} country={country}></Country>)
                 }
             </div>
         </div>
--- recape-rest-countries/src/components/Country/Country.jsx
+++ recape-rest-countries/src/components/Country/Country.jsx
@@ -3,30 +3,33 @@ import './Country.css'
 import PropTypes from 'prop-types';
 
 
-const Country = ({ country }) => {
-    // console.log(country);
+const Country = ({ country , handleVisitedCountries }) => {
+    // console.log(handleVisitedCountries);
     const { name, flags, population } = country;
 
     const [visited, setVisited] = useState(false);
 
     const handlerVisited = () => {
         setVisited(!visited);
-    }
+    };
+
     return (
-        <div className="country">
-            <h4>Name : {name.common}</h4>
+        <div className={`country ${visited && 'visited-bg'}`} /* style={{backgroundColor: visited && 'red'}} */> {/* comment kora line ta kaj korce but 'visited-bg' class kaj korce na */}
+            <h4 style={{ color: visited && 'blueviolet' }}>Name : {name.common}</h4>
             <img src={flags.png} alt="" />
             <p>Population : {population}</p>
-            <button onClick={handlerVisited}>{visited? 'Visited':'Going'}</button>
+            <button onClick={handlerVisited}>{visited ? 'Visited' : 'Going'}</button>
+            <button onClick={()=>{handleVisitedCountries(country)}}>Visited List</button>
             {
-                visited ? <p style={{color:'red'}}>I visited this country</p> : <p>I want to visite</p>
+                visited ? <p style={{ color: 'red' }}>I visited this country</p> : <p>I want to visite</p>
             }
         </div>
     );
 };
 
-Country.propTypes={
-    country:PropTypes.object
+Country.propTypes = {
+    country: PropTypes.object,
+    handleVisitedCountries: PropTypes.func
 }
 
 export default Country;
